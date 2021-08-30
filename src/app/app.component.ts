50712a03d3f05eabc1ce1bb9e0156029c140e709
hemanth@proveitcatalysts.com <hemanth@proveitcatalysts.com>
2021-08-31 01:44:13 +0530

    reactiveForms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators} from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'assignemnt';
+   signInForm : any =  FormGroup;
+   result_data : any = [];
+   searchValue : any = '';
+
+  constructor( private formBuilder: FormBuilder, private service : HttpClient){}
+  
+  ngOnInit(){
+    this.signInForm = this.formBuilder.group({
+      name : ['',[Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+      contact: ['', [Validators.required ]]
+    });
+    this.service.get('../assets/data.json').subscribe((res: any ) =>{
+      console.log("Res_data", res)
+      this.result_data = res
+    });
+  }
+
+  onSubmit() {
+    if (this.signInForm.status === 'INVALID') {
+      console.log("Hi", this.signInForm )
+        alert("Please enter all details")
+        
+    }else{
+      console.log("Hi", this.signInForm.value)
+      let obj ={
+        name: this.signInForm.value.name,
+        email: this.signInForm.value.email,
+        password : this.signInForm.value.password,
+        contact: this.signInForm.value.contact
+      }
+      this.result_data.push(obj);  
+      this.signInForm.reset();
+    }
+  }
+
+ 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http'
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    FormsModule,
+    Ng2SearchPipeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
